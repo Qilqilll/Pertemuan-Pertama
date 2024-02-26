c627e112b74137853195dffbc3a2d0f9f97c5dff
siapa <siapa@QILQILLL>
2024-02-26 11:30:23 +0700

    Add project files.

---
+++ Pertemuan Pertama/Pertemuan Pertama.cpp
@@ -0,0 +1,18 @@
+// Pertemuan Pertama.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ //begin
+	// numeric nJejari, nLuas
+	// display 'Masukan jejari= '
+	// accept nJejari
+	// compute nLuas = 3,14 * r * r
+	// display 'Luasnya= ' + nLuas
+//end
+	double nJejari, nLuas;
+	cout << "Masukan Jari = ";
+	system("pause");
+}
